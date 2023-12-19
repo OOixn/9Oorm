a0975111fdb229cc8c3994f35cc3cf4debdfc7e2
O.O_ixn <gmltjr000@gmail.com>
2023-12-19 16:48:09 +0900

    컴포턴트 스캔과 의존관계 자동 주입 시작

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("memberService2")// 빈의 기본 이름은 클래스의 맨 앞 글자를 소문자로 변경하여 사용하지만, 직접 빈 이름을 지정 가능하다
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired // 생성자에 지정해주면 스프링 컨테이너가 자동으로 해당 스프링 빈을 찾아서 주입한다 기본은 타입이 같은 빈을 찾아서 주입한다
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
