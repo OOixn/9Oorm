1a84c0a3f9ea02c8b25495be10539d1519da390c
O.O_ixn <gmltjr000@gmail.com>
2023-12-20 10:54:28 +0900

    필드 주입

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,33 +3,27 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-    private  MemberRepository memberRepository;
-    private  DiscountPolicy discountPolicy;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private DiscountPolicy discountPolicy;
 
-//    수정자 주입 - setter라 불리는 필드의 값을 변경하는 수정자 메서드를 통해 의존관계를 주입하는 방법.
-//    선택, 변경 가능성이 있는 의존관계에 사용.
-//    자바빈 프로퍼티 규약의 수정자 메서드 방식ㅇ르 사용하는 방법.
-
-    @Autowired(required = false) // 선택적으로 할 때, or.
     public void setMemberRepository(MemberRepository memberRepository) {
-        System.out.println("memberRepository = " + memberRepository);
         this.memberRepository = memberRepository;
     }
 
-    @Autowired
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        System.out.println("discountPolicy = " + discountPolicy);
         this.discountPolicy = discountPolicy;
     }
+//    필드 주입 - 필드에 바로 주입 하는 방법
+//    코드가 간결해서 많은 개발자들을 유혹하지만 외부에서 변경이 불가능해서 테스트하기 힘들다는 치명적인 단점이 있다.
+//    DI 프레임 워크가 없으면 아무것도 할 수 없다.
+//    사용하지 말자!
 
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl() {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
