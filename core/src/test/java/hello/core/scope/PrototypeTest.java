494e04a2cd79d3204b0254668de88c26b5a94f8c
O.O_ixn <gmltjr000@gmail.com>
2023-12-21 16:34:52 +0900

    싱글톤 빈과 함께 사용시 문제점

--- core/src/test/java/hello/core/scope/PrototypeTest.java
+++ core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -10,9 +10,6 @@ import org.springframework.context.annotation.Scope;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-// 스프링 컨테이너에 요청 할 때마다 새로 생성,
-// 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입 그리고 초기화까지만 관여한다.
-// 종료 메서드가 호출 되지 않는다. 조회한 클라이언트가 관리 해야 한다. 종료 메서드에 대한 호출도 직접 해야 한다.
 
 public class PrototypeTest {
     @Test
