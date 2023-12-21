1fc63def4836cfd610dd8a17155e445ca1c5454b
O.O_ixn <gmltjr000@gmail.com>
2023-12-21 16:46:24 +0900

    문제 해결 방법1 ObjectProvider

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -36,19 +37,21 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입! 그래서 계속 같은 프로토타입 빈을 사용하게 된다.
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        // 스프링 컨테이너에서 지정한 빈을 대신 찾아주는 기능이라고 생각하면 편하다.
+        // 딱 필요한 DL 정도의 기능만 제공한다.
+        // ObjectFactory보다 좋은 점 상속, 옵션, 스트림 처리 등 편의 기능이 많고, 별도의 라이브러비가 필요없다,
+        // import org.springframework.beans.factory.ObjectProvider; 스프링에 의존한다.
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
