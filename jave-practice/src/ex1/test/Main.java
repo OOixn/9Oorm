c127722a2e488b57f7e5db0deaf18213b676f070
O.O_ixn <gmltjr000@gmail.com>
2024-03-04 12:00:00 +0900

    수정

--- jave-practice/src/ex1/test/Main.java
+++ jave-practice/src/ex1/test/Main.java
@@ -2,7 +2,7 @@ package ex1.test;
 
 public class Main {
     public static void main(String[] args) {
-        Abc1 abc = new Abc1("서울");
+        Abc1 abc = new Abc1("경기");
 abc.intro();
 abc.takeOrder();
     }
